a547806e368700914c84442ad4c40e484f6e22a4
wellington lucas ferreira morales <wellucas151999@gmail.com>
2022-02-26 17:14:39 -0300

    Semana Origin Rocktseat-aula 2

---
+++ main.js
@@ -0,0 +1,34 @@
+//Bre e fecha o menu quando clicar no icone: hamburgue e x
+//queryselector ele pega a semelhança de header e nav
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show') //ele está pegando o nav e olhando a lista de classes se tiver o show vc tira , se não tiver vc coloca
+    //toggle literalmente add na class um show
+  })
+}
+
+//QUANDO CLICAR EM QUALQUER ITEM DO MENU, ESCONDER MENU
+const link = document.querySelectorAll('nav ul li a')
+
+for (const links of link) {
+  links.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//MUDAR O HEADER DA PAGINA QUANDO ROLAR O SCROLL(ADICIONANDO UMA SOMBRA)
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
