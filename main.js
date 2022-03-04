250b1cd9dc0fcc0463646b204cc19dc5f31e05bd
wellington lucas ferreira morales <wellucas151999@gmail.com>
2022-03-04 17:37:00 -0300

    Semana Origin Rockseat- Aula 3

--- main.js
+++ main.js
@@ -34,3 +34,29 @@ window.addEventListener('scroll', function () {
 })
 
 /*  Testimonioals carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*ScrollReveal: Mostra elementos quando der scroll na página*/
+const scrollRevel = scrollRevel({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollRevel.reveal(
+  `#home .image,#home .text,
+  #about .image, #about .text,
+  #services header,#services .card,
+  #testimonial header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
