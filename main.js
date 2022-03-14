338fec30a3fb7574c2cdd56c3400e25dde06faae
wellington lucas ferreira morales <wellucas151999@gmail.com>
2022-03-14 18:42:15 -0300

    Semana Origin Six Rocktseat- Aula 5 ajustes de menu

--- main.js
+++ main.js
@@ -22,10 +22,9 @@ for (const link of links) {
 
 //MUDAR O HEADER DA PAGINA QUANDO ROLAR O SCROLL(ADICIONANDO UMA SOMBRA)
 /* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
-
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -71,9 +70,8 @@ scrollReveal.reveal(
 )
 
 /* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -81,8 +79,34 @@ function backToTop() {
   }
 }
 
+/* Menu ativo conforme a seção visual na pagina */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /* When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
